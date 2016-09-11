16dcc055ad6c4243f69a46da9976404bd3ad21fb
Michael Davidsaver <mdavidsaver@gmail.com>
2016-09-11 15:08:24 -0400

    update debian packaging

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='procServUtils',
+    description='Support scripts for procServ',
+    packages = ['procServUtils'],
+    scripts = [
+        'manage-procs',
+        'procServ-launcher',
+    ],
+)
