ralphlange/procServ

commit 50ff0b3875849bddf5f02c4a9838ea9c6558d83b

constify timeFormat

procServ.cc
@@ -65,10 +65,10 @@ time_t holdoffTime = 15;         // Holdoff time between child restarts (in seco
 pid_t  procservPid;              // PID of server (daemon if not in debug mode)
 char   *pidFile;                 // File name for server PID
 char   defaultpidFile[] = "pid.txt";  // default
-char   *timeFormat;              // Time format string
+const char   *timeFormat = "%c";       // Time format string
 char   defaulttimeFormat[] = "%c";    // default
 bool   stampLog = false;         // Prefix log lines with time stamp
-char   *stampFormat;             // Log time stamp format string
+const char   *stampFormat;             // Log time stamp format string
 
 const size_t INFO1LEN = 512;
 const size_t INFO2LEN = 128;
@@ -429,9 +429,10 @@ int main(int argc,char * argv[])
     }
 
     if (!stampFormat) {
-        stampFormat = (char*) calloc(strlen(timeFormat)+4, 1);
         if (stampFormat) {
-            sprintf(stampFormat, "[%s] ", timeFormat);
+            char *tmp = (char*) calloc(strlen(timeFormat)+4, 1);
+            sprintf(tmp, "[%s] ", timeFormat);
+            stampFormat = tmp;
         } else {
             stampFormat = timeFormat;
         }

procServ.h
@@ -38,7 +38,7 @@ extern bool   setCoreSize;
 extern char   *procservName;
 extern char   *childName;
 extern char   *ignChars;
-extern char   *timeFormat;
+extern const char   *timeFormat;
 extern char   killChar;
 extern char   toggleRestartChar;
 extern char   restartChar;

commit 06f967b8defdec7bdc410d3c5a231421ca30ae73

Fix bug introduced by 50ff0b3 (fixes #18)

procServ.cc
@@ -450,8 +450,8 @@ int main(int argc,char * argv[])
     }
 
     if (!stampFormat) {
-        if (stampFormat) {
-            char *tmp = (char*) calloc(strlen(timeFormat)+4, 1);
+        char *tmp = (char*) calloc(strlen(timeFormat)+4, 1);
+        if (tmp) {
             sprintf(tmp, "[%s] ", timeFormat);
             stampFormat = tmp;
         } else {