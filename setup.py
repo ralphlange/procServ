17a39129f19307ad561f8997f3be4a71d5488e4d
Ralph Lange <ralph.lange@gmx.de>
2019-06-14 15:28:14 +0200

    utils: correctly install systemd-generators

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 #!/usr/bin/env python
 
+import os
+import distutils.command.install_data
 from distutils.core import setup
+from distutils import log
+
+class custom_install_data(distutils.command.install_data.install_data):
+    """need to set the systemd generators to mode 0755"""
+    def run(self):
+        distutils.command.install_data.install_data.run(self)
+        install_cmd = self.get_finalized_command('install')
+        dst = getattr(install_cmd, 'install_data')
+        for type in ['user', 'system']:
+            file = os.path.join(dst, 'lib/systemd/%s-generators/systemd-procserv-generator-%s'%(type,type))
+            self.announce('changing mode of %s to 755' % file, level=log.INFO)
+            os.chmod(file, 0755)
 
 setup(
     name='procServUtils',
@@ -11,4 +25,9 @@ setup(
         'procServ-launcher',
         'prattach',
     ],
+    data_files = [
+        ('lib/systemd/system-generators', ['systemd-procserv-generator-system']),
+        ('lib/systemd/user-generators', ['systemd-procserv-generator-user']),
+    ],
+    cmdclass = { 'install_data': custom_install_data },
 )
