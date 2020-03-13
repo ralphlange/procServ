4d2b2bcbc8fe1e0586f48481b78e61d786c5df75
Kevin Peterson <kmpeters@anl.gov>
2020-03-13 17:06:13 -0500

    Set the PROCSERV_INFO env var with the info that resides in the pid and info files.  Use KEY=value format.
    Problems:
    * The PID in the env var doesn't match the one in the pid file
    * There is a trailing semicolon that should be removed

--- procServ.cc
+++ procServ.cc
@@ -12,6 +12,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <sstream>
 
 #include <stdio.h>
 #include <assert.h>
@@ -102,6 +103,7 @@ void OnPollTimeout();
 // Daemonizes the program
 void forkAndGo();
 void openLogFile();
+void setEnvVar();
 void writeInfoFile(const std::string& infofile);
 void ttySetCharNoEcho(bool save);
 
@@ -542,6 +544,8 @@ int main(int argc,char * argv[])
 
     procservPid=getpid();
 
+    setEnvVar();
+
     openLogFile();
 
     if (false == inFgMode && false == inDebugMode)
@@ -921,6 +925,16 @@ void writeInfoFile(const std::string& infofile)
     info<<"pid:"<<getpid()<<"\n";
     for(connectionItem *it = connectionItem::head; it; it=it->next)
         it->writeAddress(info);
+}
+
+void setEnvVar()
+{
+    std::ostringstream env_var;
+    env_var<<"PID="<<getpid()<<";";
+    for(connectionItem *it = connectionItem::head; it; it=it->next)
+        it->writeAddressEnv(env_var);
+    std::string env_str = env_var.str();
+    setenv("PROCSERV_INFO", env_str.c_str(), 1);
 }
 
 void ttySetCharNoEcho(bool set) {
